cdfdef719489705531c37f7c37e0b71e0352b56c
Domingos Creado <domingos.creado@gmail.com>
2014-10-04 07:15:46 -0300

    Trigger the compiler on any directory change

---
+++ WinLess/Models/CompilerDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinLess.Models
+{
+    public interface CompilerDispatcher
+    {
+        void CompileSelectedFiles();
+    }
+}
